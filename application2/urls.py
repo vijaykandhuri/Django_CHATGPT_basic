75a1eadd73d060b5bc9070770e0913aad20ed1e4
vijaykumar <vijay.kandhuri@gmail.com>
2023-09-12 11:49:51 +0530

    By using CHATGPT and Django create a web application, ask qus in django ane responce from chatgpt in the form of Json formate

---
+++ application2/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from application2.views import *
+
+urlpatterns = [
+    #path('',views.chatbot, name='chatbot')
+    
+]
---
+++ application2/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.http import HttpResponse,HttpRequest
+import openai
+
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+"""def ask_openai(message):
+    response=openai.Completion.create(
+        model = "gpt-3.5-turbo",
+        prompt = message,
+        max_tokens = 150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    
+    )
+    #print(response)
+    answer=response.choices[0].text.strip()
+    #answer = response.choices[0].message.content.strip()
+    return answer"""
+    
+def ask_openai(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt = message + "multiple choice question with Json format with options line by line " + message  ,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    print(response)  
+    answer = response.choices[0].text.strip()
+    return answer
+# Create your views here.
+
+
+
+
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message':message, 'response':response})
+    return render(request,'chatbot.html')
